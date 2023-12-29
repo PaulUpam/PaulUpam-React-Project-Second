003b604207bc08a584c74db57dc3b560ae74bcb2
PaulUpam <paulupam@gmail.com>
2023-12-30 00:32:50 +0600

    new Update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
     <h1 className="bg-yellow-400 text-4xl ">React TailwindCss DaisyUI intall done</h1>
-
+<div className="containe"><h1>cpanel</h1></div>
   
     </>
   )
